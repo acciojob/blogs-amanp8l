510f782a45572cde89bac9cfcff61d17259c412a
Aman Patel <amanpatel255025@gmail.com>
2023-02-18 17:50:16 +0530

    sol

--- src/main/java/com/driver/services/BlogService.java
+++ src/main/java/com/driver/services/BlogService.java
@@ -21,13 +21,22 @@ public class BlogService {
     @Autowired
     UserRepository userRepository1;
 
-    public Blog createAndReturnBlog(Integer userId, String title, String content) {
+    public Blog createAndReturnBlog(Integer userId, String title, String content) throws Exception {
         //create a blog at the current time
+
+        if(!userRepository1.findById(userId).isPresent()){
+            throw new Exception();
+        }
+        User user = userRepository1.findById(userId).get();
+        Blog blog = new Blog(user,title,content);
+        userRepository1.save(user); //Blog saved in repo by cascading
+        user.getBlogList().add(blog);
+        return blog;
 
     }
 
     public void deleteBlog(int blogId){
         //delete blog and corresponding images
-
+        blogRepository1.deleteById(blogId);
     }
 }
